6e8c6d5868690d264358c473bf5b10672b11b439
daisuketerashita <ekusiad.tera@icloud.com>
2021-07-10 21:33:07 +0900

    isLikedByメソッド

--- backend/app/Models/Article.php
+++ backend/app/Models/Article.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -19,5 +20,15 @@ class Article extends Model
     //リレーション
     public function user(): BelongsTo{
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function likes(): BelongsToMany{
+        return $this->belongsToMany('App\Models\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool{
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
